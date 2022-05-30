ae8c7c1beca297bc6123601246c173369122130b
Emilbou <emilien.boucard@edu.univ-fcomte.fr>
2022-05-30 17:09:01 +0200

    Page nous contacter

---
+++ src/views/Nouscontacter.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="bg-orange-50">
+    <HeaderXs />
+    <p class="p-6 font-unna text-4xl font-bold">Quelque chose à nous dire ?</p>
+
+    <Input texte1="Objet*" />
+    <Input texte1="email*" />
+    <Input texte1="Nom*" />
+    <Input texte1="Prénom*" />
+    <Message texte1="Votre Message*" />
+  </div>
+  <Footer />
+</template>
+
+<script>
+import HeaderXs from "../components/HeaderXs.vue";
+import Input from "../components/Nouscontacter/Input.vue";
+import Message from "../components/Nouscontacter/Message.vue";
+import Footer from "../components/Footer.vue";
+export default {
+  components: { HeaderXs, Input, Message, Footer },
+};
+</script>
+
+
+
+  
