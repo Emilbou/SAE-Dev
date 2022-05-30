f19cfc5a89414544011e3e9fe46215ccb8918995
Emilbou <emilien.boucard@edu.univ-fcomte.fr>
2022-05-30 17:08:43 +0200

    Page 404

---
+++ src/views/Page404.vue
@@ -0,0 +1,14 @@
+<template>
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Tailwind CSS 404 Page</title>
+    <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet" />
+  </head>
+  <body>
+    <div class="flex h-screen w-screen items-center justify-center bg-indigo-600">
+      <p class="text-5xl text-white md:text-7xl lg:text-9xl">404</p>
+    </div>
+  </body>
+</template>
