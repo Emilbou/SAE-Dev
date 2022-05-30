dfc1dbaf0185f3035153fef2b2e23edc114a24de
Emilbou <emilien.boucard@edu.univ-fcomte.fr>
2022-05-30 17:08:10 +0200

    Tickets

---
+++ src/views/Tickets.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="bg-orange-50">
+    <HeaderXs />
+    <p>Tickets</p>
+  </div>
+</template>
+
+<script>
+import HeaderXs from "../components/HeaderXs.vue";
+import Footer from "../components/Footer.vue";
+
+export default {
+  components: { HeaderXs },
+};
+</script>
