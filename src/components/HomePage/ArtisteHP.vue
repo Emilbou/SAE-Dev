bd0b0b3954d2b02eb7c8590d2dc2b5b98370a28e
Emilbou <emilien.boucard@edu.univ-fcomte.fr>
2022-05-30 17:13:32 +0200

    tous les cpmposants homepage

---
+++ src/components/HomePage/ArtisteHP.vue
@@ -0,0 +1,43 @@
+<template>
+  <div v-if="!menuOuvert" class="bg-orange-50">
+    <div class="my-9">
+      <!--partie contenu-->
+      <div class="grid justify-center">
+        <div class="ml-auto mr-auto">
+          <img :src="image1" />
+        </div>
+
+        <!--Cercle+ fleche avec router link-->
+        <div class="mx-12 -my-[70px]">
+          <RouterLink to="/Artistes">
+            <p class="-my-2 py-3 font-roboto text-3xl text-white">{{ texte1 }}</p>
+            <div class="flex h-12 w-12 items-center justify-center rounded-full bg-white">
+              <svg
+                width="30"
+                height="23"
+                viewBox="0 0 30 23"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                preserveAspectRatio="xMidYMid meet"
+              >
+                <path
+                  d="M28.8534 12.8226C29.4392 12.2368 29.4392 11.2871 28.8534 10.7013L19.3074 1.15536C18.7217 0.569574 17.7719 0.569574 17.1861 1.15536C16.6003 1.74115 16.6003 2.6909 17.1861 3.27668L25.6714 11.762L17.1861 20.2472C16.6003 20.833 16.6003 21.7828 17.1861 22.3686C17.7719 22.9544 18.7217 22.9544 19.3074 22.3686L28.8534 12.8226ZM0.0583496 13.262H27.7927V10.262H0.0583496V13.262Z"
+                  fill="black"
+                ></path>
+              </svg>
+            </div>
+          </RouterLink>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    image1: String,
+    texte1: String,
+  },
+};
+</script>
---
+++ src/components/HomePage/RetrouvezArtistes.vue
@@ -0,0 +1,25 @@
+<!--on est bon!-->
+<template>
+  <div class="p-9">
+    <div class="h-auto w-auto rounded-lg bg-zinc-600 p-9">
+      <RouterLink to="/Artistes">
+        <p class="w-auto text-center font-unna text-2xl font-bold text-white">Retrouvez tous les artistes</p>
+        <div class="m-auto flex items-center justify-center py-9 align-middle">
+          <svg
+            width="75"
+            height="24"
+            viewBox="0 0 75 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            preserveAspectRatio="xMidYMid meet"
+          >
+            <path
+              d="M74.0607 13.0607C74.6464 12.4749 74.6464 11.5251 74.0607 10.9393L64.5147 1.3934C63.9289 0.807614 62.9792 0.807614 62.3934 1.3934C61.8076 1.97919 61.8076 2.92894 62.3934 3.51472L70.8787 12L62.3934 20.4853C61.8076 21.0711 61.8076 22.0208 62.3934 22.6066C62.9792 23.1924 63.9289 23.1924 64.5147 22.6066L74.0607 13.0607ZM-2.47566e-07 13.5L73 13.5L73 10.5L2.47566e-07 10.5L-2.47566e-07 13.5Z"
+              fill="white"
+            ></path>
+          </svg>
+        </div>
+      </RouterLink>
+    </div>
+  </div>
+</template>
