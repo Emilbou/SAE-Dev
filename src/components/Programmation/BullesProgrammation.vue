1c46292c0bc17b5909536ac3a1ecb291db9e46ca
Emilbou <emilien.boucard@edu.univ-fcomte.fr>
2022-05-30 17:12:36 +0200

    création composants/ dossier programmation

---
+++ src/components/Programmation/BullesProgrammation.vue
@@ -0,0 +1,24 @@
+<template>
+  <div v-if="!menuOuvert" class="bg-orange-50">
+    <div class="my-[60px] p-7">
+      <!--partie contenu-->
+      <div class="grid justify-center">
+        <div class="ml-auto mr-auto">
+          <img class="rounded-full" :src="image1" />
+
+          <p class="-my-[60px] text-center font-roboto text-3xl text-white">{{ texte1 }}</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default {
+  props: {
+    image1: String,
+    texte1: String,
+  },
+};
+</script>
---
+++ src/components/Programmation/Dates.vue
@@ -0,0 +1,13 @@
+<template>
+  <p class="p-6 font-unna text-4xl font-bold">
+    {{ Dates }}
+  </p>
+</template>
+
+<script>
+export default {
+  props: {
+    Dates: String,
+  },
+};
+</script>
