05af6118cbdeb83fd80313261af3c0f285b2c119
Emilbou <emilien.boucard@edu.univ-fcomte.fr>
2022-05-31 08:40:11 +0200

    Grosse update responsive

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,23 +1,32 @@
 <template>
   <div class="bg-orange-50">
-    <div class="hidden sm:grid md:hidden lg:hidden xl:hidden 2xl:hidden">
-      <HeaderXs />
+    <HeaderXs />
+
+    <div class="sm:grid lg:hidden xl:hidden 2xl:hidden">
       <ArtisteHP image1="src/assets/HomePage/Rectangle10.png" texte1="The Strokes" />
       <ArtisteHP image1="src/assets/HomePage/Rectangle11.png" texte1="Tame Impala" />
       <ArtisteHP image1="src/assets/HomePage/Rectangle12.png" texte1="Cage The Elephant" />
       <RetrouvezArtistes />
-      <hr />
-      <ReservezBillets />
-      <Footer />
     </div>
 
-    <HeaderXs />
-    <div class="grid-flow-col grid-rows-3 sm:hidden md:grid lg:hidden xl:hidden 2xl:hidden">
-      <ArtisteHP class="row-span-3" image1="src/assets/HomePage/Rectangle10.png" texte1="The Strokes" />
-      <ArtisteHP class="col-span-2" image1="src/assets/HomePage/Rectangle11.png" texte1="Tame Impala" />
-      <ArtisteHP class="row-span -my-4" image1="src/assets/HomePage/Rectangle12.png" texte1="Cage The Elephant" />
+    <div class="sm:hidden lg:grid xl:hidden 2xl:hidden">
+      <div class="grid grid-flow-col grid-rows-3 gap-4">
+        <ArtisteHP class="row-span-3" image1="src/assets/HomePage/Rectangle10.png" texte1="The Strokes" />
+        <ArtisteHP class="col-span-2" image1="src/assets/HomePage/Rectangle11.png" texte1="Tame Impala" />
+        <ArtisteHP class="col-span-2 row-span-2" image1="src/assets/HomePage/Rectangle12.png" texte1="Cage The Elephant" />
+      </div>
+      <RetrouvezArtistes />
     </div>
-    <RetrouvezArtistes class="-my-9" />
+
+    <div class="sm:hidden lg:hidden xl:grid 2xl:hidden">
+      <div class="grid grid-flow-col grid-rows-3 gap-4">
+        <ArtisteHP class="row-span-3" image1="src/assets/HomePage/Rectangle10.png" texte1="The Strokes" />
+        <ArtisteHP class="col-span-2" image1="src/assets/HomePage/Rectangle11.png" texte1="Tame Impala" />
+        <ArtisteHP class="col-span-2 row-span-2" image1="src/assets/HomePage/Rectangle12.png" texte1="Cage The Elephant" />
+        <RetrouvezArtistes class="col-span-3" />
+      </div>
+    </div>
+
     <hr />
     <ReservezBillets />
     <Footer />
