c6bc6a2702824ced28977731b32166c752fb0cac
Emilbou <emilien.boucard@edu.univ-fcomte.fr>
2022-05-31 09:23:45 +0200

    Responsive page artistes

--- src/components/Artistes/Artistes.vue
+++ src/components/Artistes/Artistes.vue
@@ -1,29 +1,12 @@
 <template>
-  <div>
-    <div v-if="!menuOuvert" class="bg-orange-50">
-      <div class="my-[60px] p-7">
-        <!--partie contenu-->
-        <div class="grid justify-center">
-          <div class="ml-auto mr-auto">
-            <img class="rounded-xl" :src="image1" />
+  <div v-if="!menuOuvert" class="bg-orange-50">
+    <div class="my-[60px] p-7">
+      <!--partie contenu-->
+      <div class="grid justify-center">
+        <div class="ml-auto mr-auto">
+          <img class="rounded-xl" :src="image1" />
 
-            <p class="-my-[60px] text-center font-roboto text-4xl text-white">{{ texte1 }}</p>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-
-  <div>
-    <div v-if="!menuOuvert" class="bg-orange-50">
-      <div class="my-[60px] p-7">
-        <!--partie contenu-->
-        <div class="grid justify-center">
-          <div class="ml-auto mr-auto">
-            <img class="rounded-xl" :src="image1" />
-
-            <p class="-my-[60px] text-center font-roboto text-4xl text-white">{{ texte1 }}</p>
-          </div>
+          <p class="-my-[60px] text-center font-roboto text-4xl text-white">{{ texte1 }}</p>
         </div>
       </div>
     </div>
