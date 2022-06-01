028637deedfed368d6a5d8b71a2970385b2c66cb
Emilbou <emilien.boucard@edu.univ-fcomte.fr>
2022-06-01 15:36:14 +0200

    Firebase fonctionne pas

--- src/views/TestFirebase.vue
+++ src/views/TestFirebase.vue
@@ -1,1 +1,64 @@
-<template></template>
+<template>
+  <div class="text-white">
+    <h5 class="text-white">Liste des artistes - Simple liste</h5>
+  </div>
+  <div v-for="artiste in listeArtistes" :key="artiste.id">
+    <p class="text-white">{{ artiste.nom }}</p>
+  </div>
+  <hr />
+</template>
+<template>
+  <div class="text-white">
+    <h5 class="text-white">Liste des artistes - Simple liste</h5>
+  </div>
+  <div v-for="artiste in listeArtistes" :key="artiste.id">
+    <p class="text-white">{{ artiste.nom }}</p>
+  </div>
+  <hr />
+</template>
+
+<script>
+import { getFirestore, collection, getDocs } from "https://www.gstatic.com/firebasejs/9.7.0/firebase-firestore.js";
+
+export default {
+  data() {
+    return {
+      nom: null,
+      message: null,
+      listeArtistes: [],
+    };
+  },
+  mounted() {
+    this.getArtistes();
+  },
+  methods: {
+    async getArtistes() {
+      const firestore = getFirestore();
+      const dbArtistes = collection(firestore, "artistes");
+      const query = await getDocs(dbArtistes);
+      query.forEach((doc) => {
+        let artistes = {
+          id: doc.id,
+          nom: doc.data().nom,
+        };
+        this.listeArtistes.push(artistes);
+      });
+    },
+  },
+};
+</script>
+
+<style scoped>
+.center {
+  text-align: center;
+}
+.title {
+  font-size: 1.4rem;
+  font-weight: 500;
+  margin-bottom: 0.4rem;
+  color: #34495e;
+}
+h4 {
+  font-weight: bold;
+}
+</style>
