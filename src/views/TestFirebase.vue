2e4528d37ff9932412455c2ace7c35c3a83d99bc
Emilbou <emilien.boucard@edu.univ-fcomte.fr>
2022-06-02 08:34:09 +0200

    copier coller code fino pour adaptation

--- src/views/TestFirebase.vue
+++ src/views/TestFirebase.vue
@@ -6,6 +6,49 @@
     <p>{{ artiste.nom }}</p>
   </div>
   <hr />
+  <!--I-----------------------------------------------------------------------------I-->
+  <div>
+    <h5>Liste des pays - Liste synchronisée</h5>
+  </div>
+  <form class="mb-3">
+    <h6>Nouveau pays</h6>
+    <div class="input-group">
+      <div class="input-group-prepend">
+        <span class="input-group-text">Nom</span>
+      </div>
+      <input type="text" v-model="nom" class="form-control" required />
+      <button class="btn btn-light" type="button" @click="createPays()" title="Création">
+        <i class="fa fa-save fa-lg"></i>
+      </button>
+    </div>
+  </form>
+  <table class="table">
+    <thead class="thead-dark">
+      <tr>
+        <th scope="col">Id</th>
+        <th scope="col">Nom</th>
+        <th scope="col">Actions</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="pays in listePaysSynchro" :key="pays.id">
+        <td>{{ pays.id }}</td>
+        <td>
+          <input type="text" v-model="pays.nom" />
+        </td>
+        <td>
+          <button class="btn light" @click.prevent="updatePays(pays)">
+            <i class="fa fa-edit fa-lg"></i>
+          </button>
+          <button class="btn light" @click.prevent="deletePays(pays)">
+            <i class="fa fa-trash fa-lg"></i>
+          </button>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+  <hr />
+  <!--I-----------------------------------------------------------------------------I-->
 </template>
 
 
